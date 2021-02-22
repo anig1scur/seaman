eab35cd77be334a56b935d03f11aec193744b5c5
zhuyanxin <zhuyanxin@intern.douban.com>
2021-02-23 03:01:27 +0800

    incr_topic_count

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zGina/seaman
+
+go 1.15
---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/zgina/seaman/morestrings"
+)
+
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!sffdf"))
+	fmt.Println("vim-go")
+}
